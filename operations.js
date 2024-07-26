1cfc66708832da7c2221d07f70067a7b0d3f1784
Navair770 <navairkhan123@gmail.com>
2024-07-26 07:36:28 -0700

    Third updated code

---
+++ operations.js
@@ -0,0 +1,22 @@
+let value = 33
+let negvalue = -value
+console.log(negvalue)
+let str1 = "Navair"
+let str2 = "Khan"
+let str3 = str1 + str2 
+console.log(str3);
+console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2%3);
+console.log(2**2);
+console.log(2**3);
+console.log("2"+3);
+console.log("2"+3+2);
+console.log(2+3+"3");
+let num1,num2,num3;
+num1=num2=num3=2+2;
+console.table({num1, num2, num3});
+let gamecounter = 100;
+++gamecounter;
+console.log(gamecounter);
