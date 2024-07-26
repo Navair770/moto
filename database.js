c555043d8a95b2fd1dc1f81db6b209e6f45de5db
Navair770 <navairkhan123@gmail.com>
2024-07-26 07:57:16 -0700

    updated

---
+++ database.js
@@ -0,0 +1,13 @@
+"use stricked";
+let name = "Navair"
+console.log(name);
+console.log(typeof name)
+let score = "33"
+console.log(score)
+console.log(typeof score)
+let number = "33"
+console.log(score)
+console.log(typeof score)
+let num = "33"
+let valueInNumber = Number(num)
+console.log(typeof valueInNumber)
