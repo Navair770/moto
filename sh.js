14e24b24ac585c7e1b8cd3048b39497ddd5e744a
Navair770 <navairkhan123@gmail.com>
2024-08-01 05:53:05 -0700

    stack&heap code

---
+++ sh.js
@@ -0,0 +1,13 @@
+let myYoutubename = "Navairkhan"
+let anothername = myYoutubename
+anothername = "nk chanal"
+console.log(myYoutubename);
+console.log(anothername);
+let user1={
+    Email: "navairkhan123@gmail.com",
+    Upi: "jkDFK"
+}
+let user2=user1
+user2.Email= "NAVAIR123@GMAIL.COM"
+console.log(user1.Email);
+console.log(user2.Email);
